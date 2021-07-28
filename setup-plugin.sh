9e35b71ba345d75e0534c9d7b8eb4d4690a65539
Swaraj Phadtare <1swarajphadtare@gmail.com>
2021-07-28 13:33:00 +0530

    Setup script

---
+++ setup-plugin.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+ln -s Nethermind.MevSearcher ./NethTest/src/Nethermind/
+ln -s Nethermind.MevSearcher.Test ./NethTest/src/Nethermind/
+cp configs/* ./NethTest/src/Nethermind/Nethermind.Runner/configs
